210da4bf2666802025c0763e1b37e8077bb94e78
Anastasiya Kochkina <kochkinaanastasiya@yandex.ru>
2022-11-06 22:17:17 +0300

    Initial commit

---
+++ src/main/java/com/kochkinaHW/hw2/Libraries/Application.java
@@ -0,0 +1,13 @@
+package com.kochkinaHW.hw2.Libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
